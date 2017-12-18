859f1b44d2109cc0fead114fc4824118ba0743ae
Gonzalo Coelho <coelhogonza@gmail.com>
2017-12-18 18:40:32 -0300

    hhhh

--- src/Model/ComparadorScores.java
+++ src/Model/ComparadorScores.java
@@ -1,7 +1,6 @@
 package Model;
 import java.util.Comparator;
-//Probando el githubbddddbb no borrar esto
-// 1 2 3 4
+//ggg cambio
 public class ComparadorScores implements Comparator<Score>{
 
 	public int compare(Score s1, Score s2) {
