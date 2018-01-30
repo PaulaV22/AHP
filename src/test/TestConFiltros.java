385eddf8eea50ce28c9490cede7359c546724e0f
paulavillanueva22 <paulavillanueva.22@gmail.com>
2018-01-30 12:16:20 +1100

    test de filtros

--- src/test/TestConFiltros.java
+++ src/test/TestConFiltros.java
@@ -32,16 +32,21 @@ public class TestConFiltros {
 		pc2.set("inter100000net",1);
 		
 		Pc pc3 = new Pc();
-		pc3.set("modelo", "pc1");
-		pc3.set("ssd", 1);
-		pc3.set("capacidad", );
-		pc3.set("ram", 16000);
-		pc3.set("nucleos", 8);
-		pc3.set("directx", 9);
-		pc3.set("resolucion", 1280*720);
-		pc3.set("mhz", 2600);
+		pc3.set("modelo", "pc3");
+		pc3.set("ssd", 0);
+		pc3.set("capacidad", 1000);
+		pc3.set("ram", 3000);
+		pc3.set("nucleos", 1);
+		pc3.set("directx", 0);
+		pc3.set("resolucion", 230*232);
+		pc3.set("mhz", 1200);
 		pc3.set("internet",1);
+		
 		bd.addComputadora(pc1);
+		bd.addComputadora(pc2);
+		bd.addComputadora(pc3);
+		
+		//HACER QUE LA BASE DE DATOS DEVUELVA FILTRADAS
 	}
 	
 
